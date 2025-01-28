0d570748f78c87e11e1cc221bce31ee43663f181
Code G <egelhja@gmail.com>
2025-01-28 23:00:14 +0700

    Update sitemap config and other changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: "https://www.japandevnextjsapp.shop", // Your live production URL
+    generateRobotsTxt: true, // Enable robots.txt file
+  };
+  
+  module.exports = config;
+  
+  
